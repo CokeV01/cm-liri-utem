03b14aee8c1b3aaf451361ea80dee974643db760
CokeV01 <jvildoso@utem.cl>
2024-07-13 02:15:14 -0400

    Se  agrego la vista a los lyrics favoritos (se me habia olvidado xd)

---
+++ lib/screens/favlir_screen.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class FavlirScreen extends StatefulWidget{
+  final FileSystemEntity song;
+  const FavlirScreen({super.key, required this.song});
+
+  @override
+  _FavlirScreen createState() => _FavlirScreen();
+}
+
+class _FavlirScreen extends State<FavlirScreen>{
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+
+    );
+  }
+
+}
